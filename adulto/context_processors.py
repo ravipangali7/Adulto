c7bf6260a2d7cacc4d62651c759f65b488cd95f7
Ravi Pangali <lilathapamagar453@gmail.com>
2025-09-19 02:42:54 +0545

    Chagnes

--- adulto/context_processors.py
+++ adulto/context_processors.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from core.models import CMS, Settings, AgeVerification
+from django.db import models
+from core.models import CMS, Settings, AgeVerification, Tag
 
 
 def site_branding(request):
@@ -56,6 +57,21 @@ def cms_and_settings(request):
 		context['age_verification'] = age_verification
 	except:
 		context['age_verification'] = None
+	
+	try:
+		# Get popular tags for sub-navbar (top 10 most used tags)
+		# First try to get tags with videos, if none exist, get all tags
+		
+		# If no tags have videos, just get the first 10 tags
+		popular_tags = Tag.objects.all()
+		
+		context['popular_tags'] = popular_tags
+		context['tags_count'] = Tag.objects.count()
+		
+	except Exception as e:
+		print(f"Error getting tags: {e}")
+		context['popular_tags'] = []
+		context['tags_count'] = 0
 	
 	return context
 
